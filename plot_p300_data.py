8bc28feece8c809ca38d1ae421409bc359691512
Claire Leahy <cleahy@uvm.edu>
2024-02-13 16:13:43 -0500

    Some pseudocode for functions

--- plot_p300_data.py
+++ plot_p300_data.py
@@ -3,17 +3,101 @@
 """
 Created on Mon Feb 12 22:23:10 2024
 
-@author: ClaireLeahy
+@authors: Varshney Gentela and Claire Leahy
 """
+
+# import lab modules (Part A, Part D)
+import numpy as np
+from matplotlib import pyplot as plt
+from load_p300_data import load_training_eeg
+from plot_p300_erps import get_events, epoch_data, get_erps
+from mne.stats import fdr_correction
+import plot_topo
 
 #%% Part A: Load and Epoch the Data
 
+# function goals:
+    # load in data
+    # extract target/nontarget epochs
+    # calculate ERPs
+    
+# inputs: 
+    # subject number
+    
+# returns: 
+
 #%% Part B: Calculate & Plot Parametric Confidence Intervals
+
+# assuming normal distribution of voltages for each channel and time point
+# define 95% CI based on SE of mean
+
+# function goals:
+    # plot ERPs on each channel for target/nontarget events
+    # plot confidence intervals as error bars
+
+# inputs:
+    # target_erp
+    # nontarget_erp
+    
+# returns:
+    # none?
 
 #%% Part C: Bootstrap P Values
 
+# null hypothesis: no difference between trial types
+# each bootstrapping iteration has same number of target/nontarget trials as real data
+
+# function goals:
+    # 3000 bootstrapping iterations to find distribution of target-minus-nontarget ERPs
+        # number of iterations can be input, default 3000
+    # calculate p value for each time point on each channel
+        # p value: chance that absolute value as high as that of real data observed by chance
+
+# inputs:
+    # iterations (optional), default 3000
+    # target_erp?
+    # nontarget_erp?
+    
+# returns:
+    # p value?
+
+
 #%% Part D: Plot FDR-Corrected P Values
+
+# False Discovery Rate correction to correct p values for multiple comparisons
+
+# function goals:
+    # add black dot on x-axis (ERP and CI) when ERP difference is significant at FDR-correct p value 0.05
+    
+# inputs:
+    
+# returns:
 
 #%% Part E: Evaluate Across Subjects
 
+# function goals:
+    # loop through above code on subjects 3-10
+    # save different image file for each
+    # record time channels/time points that pass FDR-corrected significance threshold
+    # make/save new subplots (for each channel) to show number of subjects that passed significance threshold at each time point and channel
+    
+# inputs:
+    # subject array (optional), default array np.arange(3,11)
+
+# returns:
+    # none?
+    
+
 #%% Part F: Plot a Spatial Map
+
+# function goals:
+    # get group median ERP across all trials 
+    # get subject median ERP across all trials
+    # assign channels spatial location
+    # use spatial location assignments as input to plot_topo.plot_topo() function
+    # use above function to plot spatial distribution of median voltages in N2 time range (one plot) and P3b time range (second plot)
+    # adjust channel name order to obtain reasonable output
+
+# inputs:
+
+# returns:
