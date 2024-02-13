28d08170daab8f063d53247ff41b3dca100ee808
Claire Leahy <cleahy@uvm.edu>
2024-02-13 10:05:29 -0500

    Cell headers and test script created

--- plot_p300_data.py
+++ plot_p300_data.py
@@ -8,4 +8,12 @@ Created on Mon Feb 12 22:23:10 2024
 
 #%% Part A: Load and Epoch the Data
 
-# Part B: 
+#%% Part B: Calculate & Plot Parametric Confidence Intervals
+
+#%% Part C: Bootstrap P Values
+
+#%% Part D: Plot FDR-Corrected P Values
+
+#%% Part E: Evaluate Across Subjects
+
+#%% Part F: Plot a Spatial Map
---
+++ test_plot_p300_data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 10:03:54 2024
+
+@author: ClaireLeahy
+"""
+
+# calling the functions defined in the module
